79d5816714d31f97a6928351216c615410414594
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2017-09-17 14:48:02 -0400

    drawing curves and stuff using examples from msdn

--- Animation Test/Form1.Designer.vb
+++ Animation Test/Form1.Designer.vb
@@ -22,20 +22,12 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'PictureBox1
-        '
-        Me.PictureBox1.Image = Global.Animation_Test.My.Resources.Resources.Chase
-        Me.PictureBox1.Location = New System.Drawing.Point(259, 114)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(114, 108)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
-        Me.PictureBox1.TabIndex = 0
-        Me.PictureBox1.TabStop = False
         '
         'Button1
         '
@@ -45,16 +37,38 @@ Partial Class Form1
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(217, 164)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(184, 144)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        Me.PictureBox2.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Animation_Test.My.Resources.Resources.Chase___Small
+        Me.PictureBox1.Location = New System.Drawing.Point(36, 164)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(129, 122)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Visible = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(740, 354)
+        Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.PictureBox1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
@@ -62,4 +76,5 @@ Partial Class Form1
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
--- Animation Test/Form1.vb
+++ Animation Test/Form1.vb
@@ -6,6 +6,10 @@
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         PaintCounter = 0
+
+
+
+
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
@@ -15,5 +19,103 @@
     Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
         PaintCounter += 1
         Debug.WriteLine("Form1_Paint #" & PaintCounter)
+
+        'DrawingStuff()
+        'BeginContainerVoid(e)
+        'FillPieRectangle(e)
+
+        DrawBezierPoint(e)
     End Sub
+
+    Private Sub DrawBezierPoint(ByVal e As PaintEventArgs)
+
+        Dim randompt As New Random
+
+        Dim pt As Integer = randompt.Next(200)
+
+        ' Create pen.
+        Dim blackPen As New Pen(Color.Black, 3)
+
+        ' Create points for curve.
+        Dim start As New Point(100, 100)
+        Dim control1 As New Point(pt, 10)
+        Dim control2 As New Point(350, 50)
+        Dim [end] As New Point(500, 100)
+
+        ' Draw arc to screen.
+        e.Graphics.DrawBezier(blackPen, start, control1, control2, [end])
+    End Sub
+
+    Sub DrawingStuff()
+
+        Dim PictureBox2Graphic As Graphics
+
+        PictureBox2Graphic = PictureBox2.CreateGraphics()
+
+        PictureBox2Graphic.DrawImage(PictureBox1.Image, New Point(10, 10))
+
+        PictureBox2Graphic.RotateTransform(90)
+
+        Dim PictureBox2DrawingMatrix As Drawing2D.Matrix
+        PictureBox2DrawingMatrix = PictureBox2Graphic.Transform
+
+        'PictureBox2DrawingMatrix.
+        'System.Drawing.Drawing2D.GraphicsContainer
+        'Dim AGraphicsContainer As New Drawing2D.GraphicsContainer
+        'AGraphicsContainer = PictureBox2Graphic.BeginContainer(PictureBox2.Bounds, PictureBox1.Bounds, GraphicsUnit.Pixel)
+        'PictureBox2Graphic.EndContainer()
+
+
+    End Sub
+
+    Private Sub BeginContainerVoid(ByVal e As PaintEventArgs)
+
+        ' Begin graphics container.
+        Dim containerState As Drawing2D.GraphicsContainer = e.Graphics.BeginContainer()
+
+        ' Translate world transformation.
+        e.Graphics.TranslateTransform(100.0F, 100.0F)
+
+        ' Fill translated rectangle in container with red.
+        '        e.Graphics.FillRectangle(New Drawing.TextureBrush(PictureBox1.Image, Drawing2D.WrapMode.TileFlipXY), 0, 0, 200, 300)
+
+        'Dim containerState1 As Drawing2D.GraphicsContainer = e.Graphics.BeginContainer()
+
+        '' Translate world transformation again
+        'e.Graphics.TranslateTransform(200.0F, 50.0F)
+
+        '' Fill translated rectangle in container1 with blue
+        'e.Graphics.FillRectangle(New Drawing.SolidBrush(Color.Blue), New Rectangle(10, 0, 30, 50))
+
+        'e.Graphics.EndContainer(containerState1)
+
+        'e.Graphics.FillRectangle(New Drawing.SolidBrush(Color.Aqua), New Rectangle(10, 0, 70, 70))
+
+        e.Graphics.FillPie(New Drawing.SolidBrush(Color.Purple), New Rectangle(0, 0, 20, 20), 10, 45)
+
+
+        ' End graphics container.
+        e.Graphics.EndContainer(containerState)
+
+        ' Fill untransformed rectangle with green.
+        e.Graphics.FillRectangle(New SolidBrush(Color.Green), 0, 0, 200, 200)
+
+    End Sub
+
+    Public Sub FillPieRectangle(ByVal e As PaintEventArgs)
+
+        ' Create solid brush.
+        Dim redBrush As New SolidBrush(Color.Red)
+
+        ' Create rectangle for ellipse.
+        Dim rect As New Rectangle(0, 0, 200, 100)
+
+        ' Create start and sweep angles.
+        Dim startAngle As Single = 0.0F
+        Dim sweepAngle As Single = 45.0F
+
+        ' Fill pie to screen.
+        e.Graphics.FillPie(redBrush, rect, startAngle, sweepAngle)
+    End Sub
+
 End Class
